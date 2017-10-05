4ff5a4b247cbceeeca3fa22ab931bbe92d75ca38
Kowus <barnabas@elite-education.org>
2017-10-05 13:49:40 +0000

    process.argv

---
+++ 01-node-core/02-the-process-object/app.js
@@ -0,0 +1,5 @@
+// The process object allows us to work with instances of
+// the current process
+// It collects all the information from the application and the command line when the application starts
+
+console.log(process.argv);
