219efbf60e382e55ebda8a46a7d2fb6a4c3ba821
Kowus <barnabas@elite-education.org>
2017-10-05 13:58:24 +0000

    process.argv

--- 01-node-core/02-the-process-object/app.js
+++ 01-node-core/02-the-process-object/app.js
@@ -2,4 +2,21 @@
 // the current process
 // It collects all the information from the application and the command line when the application starts
 
-console.log(process.argv);
+// The argv function returns the arguments passed in the terminal
+// when the application started
+// console.log(process.argv);
+
+function grab(flag) {
+    var index = process.argv.indexOf(flag);
+    return (index === -1) ? null : process.argv[index + 1];
+}
+
+var greeting = grab('--greeting');
+var user = grab('--user');
+
+if (!user || !greeting) {
+    console.log("You Blew it!");
+} else {
+    console.log(`Welcome ${user}, ${greeting}`)
+}
+
