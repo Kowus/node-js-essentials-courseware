da7e8c7a867314f9d93895e695cb06624e244d78
Kowus <barnabas@elite-education.org>
2017-10-10 22:34:47 +0000

    Global timing functions

--- 01-node-core/04-global-timing-functions/app.js
+++ 01-node-core/04-global-timing-functions/app.js
@@ -1,7 +1,30 @@
-var waitTime = 3000;
+var readLine = require('readline');
+var waitTime = 30000;
+var currentTime = 0;
+var waitInterval = 10;
+var percentWaited = 0;
+function writeWaitingPercent(p) {
+    readLine.cursorTo(process.stdout, 0);
+    process.stdout.write(`waiting ... ${p}%`)
+}
 
-console.log("Wait for it ");
+
+
+var interval = setInterval(function () {
+    currentTime += waitInterval;
+
+    percentWaited = Math.floor((currentTime/waitTime)*100);
+    writeWaitingPercent(percentWaited);
+}, waitInterval);
+
 
 setTimeout(function () {
-    console.log(`I just wasted ${waitTime/1000} seconds of your life.`)
+    clearInterval(interval);
+    // console.log(`I just wasted ${waitTime/1000} seconds of your life.`)
+    writeWaitingPercent(100);
+    console.log("\n\ndone\n\n");
 }, waitTime);
+
+
+process.stdout.write("\n\n");
+writeWaitingPercent(percentWaited);
