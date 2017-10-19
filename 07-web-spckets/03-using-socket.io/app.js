be1f043ea6833f5209f22668e2730a35f4dc591a
Kowus <barnabas@elite-education.org>
2017-10-19 00:53:04 +0000

    socket.io

---
+++ 07-web-spckets/03-using-socket.io/app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var http = require('http');
+var app = express();
+var server = http.createServer(app).listen(3000);
+var io = require('socket.io')(server);
+app.use(express.static('./public'));
+
+io.on('connection', function (socket) {
+
+    socket.on('chat', function (message) {
+        socket.broadcast.emit('message', message)
+    });
+
+    socket.emit('message', 'Hello Kakro');
+});
+
+console.log('Socket open on http://localhost:3000');
